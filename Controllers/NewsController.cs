087c747934dfee8d23dde68dbdf02be6fed7365d
PiotrKruk99-d <biuro@liberezo.pl>
2022-05-23 22:45:27 +0200

    news list in edit window, html tags removing and new lines in news

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using homePage2.Models;
+using System.Text.RegularExpressions;
 
 namespace homePage2.Controllers;
 
@@ -11,7 +12,19 @@ public class NewsController : Controller
     [Authorize(Roles = "admin")]
     public IActionResult NewsEdit()
     {
-        ViewBag.news = LiteDBOper.GetAllArticles();
+        //ViewBag.news = LiteDBOper.GetAllArticles();
+        var news  = LiteDBOper.GetAllArticles();
+
+        var newsList = news == null ? new List<Article>() : new List<Article>(news);
+
+        for (var i = 0; i < newsList.Count; i++)
+        {
+            newsList[i].Content = Regex.Replace(newsList[i].Content, "<.*?>", string.Empty);
+            newsList[i].Content = newsList[i].Content.Replace(Environment.NewLine, "<br>");
+        }
+
+        ViewBag.news = newsList;
+
         return View();
     }
 
